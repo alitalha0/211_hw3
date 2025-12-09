c5a74e72c1d996ac51c2be8078e071f9d5cd815c
alitalha0 <150053747+alitalha0@users.noreply.github.com>
2025-12-09 22:00:22 +0300

    Delete 211_hw3/src directory

--- 211_hw3/src/penguingame/objects/ITerrainObject.java
+++
@@ -1,9 +0,0 @@
-package penguingame.objects;
-import penguingame.util.Position;
-
-public interface ITerrainObject {
-    Position getPosition();
-    void setPosition(Position position);
-    String getDisplaySymbol();
-    
-}
