8b6d5849591e97cc23c1ba8256ca2c21da3a0ad7
alitalha0 <150053747+alitalha0@users.noreply.github.com>
2025-12-11 22:37:02 +0300

    Delete 211_hw3/src directory

--- 211_hw3/src/penguingame/objects/IHazard.java
+++
@@ -1,9 +0,0 @@
-package penguingame.objects;
-import penguingame.enums.Direction;
-import penguingame.terrain.CollisionResult;
-import penguingame.terrain.IcyTerrain;
-
-// Hazard interface -> all hazards must handle collision.
-public interface IHazard extends ITerrainObject {
-    CollisionResult handleCollision(ITerrainObject collider, IcyTerrain terrain, Direction dir);
-}
